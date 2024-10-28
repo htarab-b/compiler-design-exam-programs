230a66bb519a05bc0772ca13e2606cfe5ecde261
Barath B <barath@Baraths-MacBook-Air.local>
2024-10-29 00:43:38 +0530

    Made it simple

--- shift_reduce.py
+++ shift_reduce.py
@@ -1,10 +1,3 @@
-# Grammar:
-# E -> E + E
-# E -> E * E
-# E -> ( E )
-# E -> id
-
-# Define parsing rules and actions
 grammar = ["id", "E+E", "E*E", "(E)"]
 
 def shift_reduce_parse(input_string):
@@ -14,25 +7,18 @@ def shift_reduce_parse(input_string):
     print("STACK \t INPUT \t ACTION")
 
     while input_list:
-        # Shift step
         symbol = input_list.pop(0)
         stack.append(symbol)
         print("$" + ''.join(stack) + "\t" + ''.join(input_list) + "$\tSHIFT " + symbol)
         
-        # Reduce step
-        while True:
-            for rule in grammar:
-                rule_len = len(rule)
-                if ''.join(stack[-rule_len:]) == rule:
-                    for _ in range(rule_len):
-                        stack.pop()  # Remove symbols of the rule
-                    stack.append('E')  # Reduce to 'E'
-                    print("$" + ''.join(stack) + "\t" + ''.join(input_list) + "$\tREDUCE TO E (" + rule + ")")
-                    break
-            else:
-                break
-
-# Test the parser
+        for rule in grammar:
+            rule_len = len(rule)
+            if ''.join(stack[-rule_len:]) == rule:
+                for _ in range(rule_len):
+                    stack.pop()
+                stack.append('E')
+                print("$" + ''.join(stack) + "\t" + ''.join(input_list) + "$\tREDUCE TO E (" + rule + ")")
+                
 print("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id")
 input_string = input("Enter input string: ")
 shift_reduce_parse(input_string)
